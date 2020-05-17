976ea082afbbee458608cd0a3916b7c4a04b6bc2
rustembek <rustembek98@gmail.com>
2020-05-18 00:47:59 +0600

    Auto stash before rebase of "origin/Rustembek"
    Router added

--- exam-front/src/app/app-routing.module.ts
+++ exam-front/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LandingComponent} from "./landing/landing.component";
+import {RegistrationComponent} from "./registration/registration.component";
+import {LoginComponent} from "./login/login.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+
+  {path: '', component: LandingComponent},
+  {path: 'registration', component: RegistrationComponent},
+  {path: 'login', component: LoginComponent},
+
+  //{path: '', component:},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
